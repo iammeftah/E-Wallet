868b3f92adab3817d663160b6dc6c766f452da4a
itserror808 <meftahahmedreda02@gmail.com>
2024-12-18 11:27:15 +0100

    Fixing details in client space

--- ewalletfrontend/src/app/pages/authentication-page/authentication-page.component.ts
+++ ewalletfrontend/src/app/pages/authentication-page/authentication-page.component.ts
@@ -4,11 +4,12 @@ import { RouterModule, Router } from '@angular/router';
 import { SignInComponent } from '../../components/signing-forms/sign-in/sign-in.component';
 import { SignUpAgentComponent } from '../../components/signing-forms/sign-up-agent/sign-up-agent.component';
 import { SignUpClientComponent } from '../../components/signing-forms/sign-up-client/sign-up-client.component';
+import {DarkmodeTogglerComponent} from '../../components/elements/darkmode-toggler/darkmode-toggler.component';
 
 @Component({
   selector: 'app-authentication-page',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, DarkmodeTogglerComponent],
   templateUrl: './authentication-page.component.html',
   styleUrls: ['./authentication-page.component.css']
 })
