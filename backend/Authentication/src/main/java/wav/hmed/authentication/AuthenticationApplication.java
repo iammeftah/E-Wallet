56971c0bb134fa6a338bbf34edba61454887bcdc
itserror808 <meftahahmedreda02@gmail.com>
2025-01-03 19:55:48 +0100

    Started Authentication/Backoffice

--- backend/Authentication/src/main/java/wav/hmed/authentication/AuthenticationApplication.java
+++ backend/Authentication/src/main/java/wav/hmed/authentication/AuthenticationApplication.java
@@ -2,8 +2,10 @@ package wav.hmed.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:.env")
 public class AuthenticationApplication {
 
     public static void main(String[] args) {
